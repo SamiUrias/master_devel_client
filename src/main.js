122829952b0b51c37a22a9676faf374633c7335f
Samuel Urias <msamuelurias@gmail.com>
2020-06-09 13:47:52 -0600

    Creation of the credentials form to make put to the backend

--- src/main.js
+++ src/main.js
@@ -1,19 +1,19 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 
 /** Css import */
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
 
 // Install BootstrapVue
-Vue.use(BootstrapVue)
+Vue.use(BootstrapVue);
 // Optionally install the BootstrapVue icon components plugin
-Vue.use(IconsPlugin)
+Vue.use(IconsPlugin);
 Vue.config.productionTip = false;
 
 new Vue({
   router,
-  render: h => h(App)
+  render: (h) => h(App),
 }).$mount("#app");
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,65 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="container">
+      <div class="row d-flex justify-content-center">
+        <b-form>
+          <b-form-row>
+            <label class="mr-sm-2" for="inline-form-custom-select-pref"
+              >Key</label
+            >
+            <b-input v-model="key"></b-input>
+          </b-form-row>
+
+          <b-form-row>
+            <label class="mr-sm-2">Shared Secret</label>
+            <b-input v-model="shared_secret"></b-input>
+          </b-form-row>
+
+          <b-form-row>
+            <b-button
+              class="mt-3 w-100"
+              variant="primary"
+              @click="putCredentials"
+              >PUT</b-button
+            >
+          </b-form-row>
+
+          <!-- Response -->
+          <b-form-row class="mt-5">
+            <label class="mr-sm-2 font-weight-bolder">Response</label>
+            <b-textarea class="" v-model="response"></b-textarea>
+          </b-form-row>
+        </b-form>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import axios from "axios";
 export default {
   name: "Home",
-  components: {
-    HelloWorld
-  }
+  data: function () {
+    return {
+      key: "",
+      shared_secret: "",
+      response: "",
+    };
+  },
+  methods: {
+    async putCredentials() {
+      console.log("Putting...");
+      console.log(process.env.VUE_APP_HOST);
+      const response = await axios.put(
+        process.env.VUE_APP_HOST + "/credentials",
+        {
+          key: this.key,
+          shared_secret: this.shared_secret,
+        }
+      );
+    },
+  },
+  components: {},
 };
 </script>
